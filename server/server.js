6444bb30fed6c40ba26f762a09076de666be6161
NTAKIRUTIMANA Steven <sntakirutimana72@gmail.com>
2020-05-15 12:38:57 +0200

    collection some technical errors

--- server/server.js
+++ server/server.js
@@ -1,22 +1,22 @@
+import path from 'path';
+import dotenv from 'dotenv';
 import express from 'express';
 import routes from './routes';
 
+dotenv.config();
+
 const app = express();
+app.set('view engine', 'ejs');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.use((req, res, next) => {
-  next();
-});
-
 app.use((error, res, req, next) => {
-  console.log('\n\n<<<<error>>>>\n\n');
   next();
 })
 
 app.use('/api/v1', routes);
-app.use('/UI', express.static(
-  '/home/nuru/Desktop/Andela Cycle-9/way-farer/UI'));
+app.use('/UI', express.static(path.join(__dirname, '..', 'UI')));
+app.use('/', (req, res) => res.render(path.join(__dirname, '..', 'UI', 'views', 'index')));
 
 export default  app;
